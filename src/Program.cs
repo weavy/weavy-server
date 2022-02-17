80b9bcddaa03d555dbba8e22ef762c6f518e9cf8
Linus Birgerstam <linus@birgerstam.se>
2022-02-17 15:16:15 +0100

    Added src

---
+++ src/Program.cs
@@ -0,0 +1,22 @@
+using System;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Serilog;
+
+Log.Logger = new LoggerConfiguration().WriteTo.Console().CreateBootstrapLogger();
+Log.Information("Starting up");
+try {
+    var builder = WebApplication.CreateBuilder(args);
+    builder.AddWeavy(opts => opts
+        .AddAdmin()
+        .AddDropin()
+    );
+    var app = builder.Build();
+    app.UseWeavy();
+    app.Run();
+} catch (Exception ex) {
+    Log.Fatal(ex, "Unhandled exception");
+} finally {
+    Log.Information("Shutdown complete");
+    Log.CloseAndFlush();
+}
